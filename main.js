e18bcc756b216e3638f851f36c2d392bc1e28182
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-02-14 16:35:50 -0300

    Organizando um pouco melhor o main.js

--- main.js
+++ main.js
@@ -1,106 +1,80 @@
-var div = document.getElementById('box'),
-estilo = window.getComputedStyle(div),
-podeCres = false, podeMov = false,
+// VARIÁVEIS
+var div = document.getElementById('box'), 
 btnPodeMover = document.getElementById('btnPodeMover'),
-btnPodeCres = document.getElementById('btnPodeCres')
+btnPodeCres = document.getElementById('btnPodeCres'),
+estilo = window.getComputedStyle(div),
+podeCres = false, podeMov = false
 
-function zoom(event) {
+//FUNÇÕES DE ZOOM
+
+function zoom(e) {
     if (podeCres == true) {
-        let delta = Math.sign(event.deltaY)
-        if (delta < 0) {
+        let delta = Math.sign(e.deltaY)
+        if (delta < 0 || e.key == '+') {
             maisZoom(0.05)
-        }else{
+        } else if (delta > 0 || e.key == '-'){
             menosZoom(0.05)
         }
     } else {
-        console.log('Bloqueado')
+        console.log('Zoom bloqueado')
     }
 }
 
-let maisZoom = zoom => {
-if (podeCres == true) {
-        //pega o nivel de zoom atual da div
-        zoomAtt()
-    
-        //verifica se o zoom atual ultrapassou o limite (5)
+function maisZoom(zoom) {
+    zoomAtt()
+    if (podeCres == true) {
         if (Number(zoomAtual)+Number(zoom) >= 5) {
             div.style.zoom = 5
-        }else{
+        } else {
             div.style.zoom = Number(zoomAtual)+Number(zoom)
-            //console.log(zoomAtual)
         }
-} else {
-    console.log('Bloqueado')
-}
+    } else {
+        console.log('Zoom bloqueado')
+    }
 }
 
-//Função diminuir zoom
-let menosZoom = zoom => {
+function menosZoom(zoom) {
+    zoomAtt()
     if (podeCres == true) {
-        zoomAtt()
-    
-        //Verifica se o zoom atual é menor que o limite (0.1)
-        if (Number(zoomAtual)-Number(zoom) <= 0) {
+        if (Number(zoomAtual)+Number(zoom) <= 0) {
             div.style.zoom = 0.05
-            //console.log(zoomAtual)
         } else {
             div.style.zoom = Number(zoomAtual)-Number(zoom)
         }
     } else {
-        console.log('Bloqueado')
+        console.log('Zoom bloqueado')
     }
 }
 
-window.addEventListener('keydown', event => {
-    let tecla = event.key
-    if (tecla === '+') {
-        maisZoom(0.2)
-    } else if(tecla === '-'){
-        menosZoom(0.2)
-    }
-})
+function zoomAtt() {
+    return zoomAtual = estilo.getPropertyValue('zoom')
+}
 
-let zoomAtt = a =>{ return zoomAtual = estilo.getPropertyValue('zoom')}
-
-div.onwheel = zoom
-
-window.onload = addListeners
-
-function addListeners(){
+// FUNÇÕES DE MOVIMENTAÇÃO
+function addListeners() {
     div.addEventListener('mousedown', mouseDown, false)
     window.addEventListener('mouseup', mouseUp, false)
-
 }
 
-function mouseUp()
-{
+function mouseUp() {
     window.removeEventListener('mousemove', divMove, true)
 }
 
-function mouseDown(e){
-  window.addEventListener('mousemove', divMove, true)
+function mouseDown() {
+    window.addEventListener('mousemove', divMove, true)
 }
 
 function divMove(e) {
-if (podeMov == true) {
-    div.style.position = 'absolute'
-    div.style.top = e.clientY + 'px'
-    div.style.left = e.clientX + 'px'
-} else {
-    console.log('Bloqueado')
-}
-}
-
-function podeMover() {
     if (podeMov == true) {
-        podeMov = false
-        btnPodeMover.style.backgroundColor = 'red'
+        div.style.position = 'absolute'
+        div.style.top = e.clientY + 'px'
+        div.style.left = e.clientX + 'px'
     } else {
-        podeMov = true
-        btnPodeMover.style.backgroundColor = 'green'
+        console.log('Movimentação bloqueada')
     }
 }
 
+//FUNÇÃO DE BLOQUEIO
 function pode(btn) {
     if (btn == 'btnPodeMover') {
         if (podeMov == true) {
@@ -109,7 +83,7 @@ function pode(btn) {
         } else {
             podeMov = true
             btnPodeMover.style.backgroundColor = 'green'
-        }        
+        }
     } else {
         if (podeCres == true) {
             podeCres = false
@@ -117,6 +91,11 @@ function pode(btn) {
         } else {
             podeCres = true
             btnPodeCres.style.backgroundColor = 'green'
-        }
     }
 }
+}
+
+//EXECUTANDO ALGUMAS FUNÇÕES
+div.onwheel = zoom
+window.onload = addListeners
+window.onkeydown = zoom
